57ecbbad4d661a52662af50a08d540c69e0ac856
Pablo Roman <pablostrings@gmail.com>
2025-12-03 00:15:53 -0300

    fix: image size

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -2,15 +2,23 @@ function Staff() {
   return (
     <section id="staff">
       <h1>Staff</h1>
-      <h3>Pablo Roman</h3>
-      <p>
-        - Encargado de todos los procesos en IKU
-        <br />- Técnico en Sonido y Post-Producción de Audio
-        <br />- Experiencia en varios estudios de grabación de Bs. As.
-        <br />- Músico guitarrista / sesionista
-      </p>
-      <img src="/img/consola.jpg" alt="Consola analógica" id="consola" />
-      <img src="/img/pablo.jpg" alt="Pablo Roman" id="pablofoto" />
+      <div className="staff-content">
+        <div className="staff-panel">
+          <img src="/img/consola.jpg" alt="Consola analógica" className="consola-img" />
+          <div className="staff-text">
+            <h3>Pablo Roman</h3>
+            <p>
+              - Encargado de todos los procesos en IKU
+              <br />- Técnico en Sonido y Post-Producción de Audio
+              <br />- Experiencia en varios estudios de grabación de Bs. As.
+              <br />- Músico guitarrista / sesionista
+            </p>
+          </div>
+        </div>
+        <div className="staff-photo">
+          <img src="/img/pablo.jpg" alt="Pablo Roman" id="pablofoto" />
+        </div>
+      </div>
     </section>
   );
 }
